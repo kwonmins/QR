2765065efb2dbdc36b13842271a8d5b9cfb97af1
Kwon <ghj3644@gmail.com>
2025-03-09 17:50:54 +0900

    Update project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
-
+app.listen(3000, () => console.log("Server ready on port 3000."));
 module.exports = router;
